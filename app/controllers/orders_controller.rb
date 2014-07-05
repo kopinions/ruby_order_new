dbecb11477998f7fa6c78c75146c83b399a7be80
jksun <jksun@thoughtworks.com>
2014-07-05 19:00:08 +0800

    return 404 when not find user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,16 @@
 class OrdersController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  before_action :get_user
+
 
   def show
     order_find = Order.find(params[:id].to_i)
     head 200
+  end
+
+  private
+  def get_user
+    @user = User.find(params[:user_id].to_i)
   end
 
   private
--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe OrdersController, :type => :controller do
   fixtures :all
   describe 'GET' do
+
     context 'with exist order' do
       before {
+        expect(User).to receive(:find).with(1).and_return(users(:sofia))
         expect(Order).to receive(:find).with(2).and_return(orders(:buy_ruby))
         get :show, user_id: 1, id: 2
       }
@@ -16,8 +18,17 @@ RSpec.describe OrdersController, :type => :controller do
 
     context 'with none exist order' do
       it 'should return 404' do
+        expect(User).to receive(:find).with(1).and_return(users(:sofia))
         expect(Order).to receive(:find).with(-1).and_raise(ActiveRecord::RecordNotFound)
         get :show, user_id: 1, id: -1
+        expect(response).to have_http_status 404
+      end
+    end
+
+    context 'with none exist user' do
+      it 'should return 404' do
+        expect(User).to receive(:find).with(1).and_raise(ActiveRecord::RecordNotFound)
+        get :show, user_id: 1, id: 2
         expect(response).to have_http_status 404
       end
     end
