c069c6fd657ab8f5b20864e7bf80888f798a8f46
jksun <jksun@thoughtworks.com>
2014-07-05 18:11:35 +0800

    return 404 when get product

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,12 @@
 class ProductController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
   def show
+    product = Product.find(params[:id].to_i)
     head 200
   end
+
+  private
+    def not_found
+      head 404
+    end
 end
--- spec/controllers/product_controller_spec.rb
+++ spec/controllers/product_controller_spec.rb
@@ -4,10 +4,21 @@ RSpec.describe ProductController, :type => :controller do
   describe 'GET' do
     context 'with product exist' do
       before {
+        expect(Product).to receive(:find).and_return(Product.new)
         get :show, id: 1
       }
       it 'should success' do
         expect(response).to have_http_status 200
+      end
+    end
+
+    context 'with product non exist' do
+      before {
+        expect(Product).to receive(:find).and_raise(ActiveRecord::RecordNotFound)
+        get :show, id: 999
+      }
+      it 'should success' do
+        expect(response).to have_http_status 404
       end
     end
   end
